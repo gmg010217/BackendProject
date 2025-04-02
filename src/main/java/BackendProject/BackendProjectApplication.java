1d47fa8476a02106cd8100a9301ed0dd88c72ce8
gmg010217 <gmg010217@gmail.com>
2025-04-02 15:47:24 +0900

    test

--- src/main/java/BackendProject/BackendProjectApplication.java
+++ src/main/java/BackendProject/BackendProjectApplication.java
@@ -8,6 +8,7 @@ public class BackendProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendProjectApplication.class, args);
+		System.out.println("Server is running");
 	}
 
 }
