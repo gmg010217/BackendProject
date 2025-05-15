d86bf539e7c1313afbdb7803e6794ea37c815475
gmg010217 <gmg010217@gmail.com>
2025-05-15 22:11:29 +0900

    MemberService - 테스트 코드 작성 완료.

--- src/test/java/BackendProject/service/MemberServiceTest.java
+++ src/test/java/BackendProject/service/MemberServiceTest.java
@@ -1,9 +1,11 @@
 package BackendProject.service;
 
 import BackendProject.domain.Member;
+import BackendProject.dto.MemberEditInfoDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -42,7 +44,8 @@ class MemberServiceTest {
     }
 
     @Test
-    void signUp() {
+    @DisplayName("회원가입_성공")
+    void signUpSuccess() {
         // given
         Member member = new Member();
         member.setEmailId("t@gmail.com");
@@ -64,28 +67,83 @@ class MemberServiceTest {
     }
 
     @Test
-    void login() {
-        // given
-
-
+    @DisplayName("로그인_성공")
+    void loginSuccess() {
         // when
-
+        Member loginMember = memberService.login(testMember.getEmailId(), testMember.getPassword());
 
         // then
-
-
+        assertThat(loginMember).isNotNull();
+        assertThat(loginMember.getEmailId()).isEqualTo(testMember.getEmailId());
+        assertThat(loginMember.getPassword()).isEqualTo(testMember.getPassword());
     }
 
     @Test
-    void info() {
+    @DisplayName("로그인_실패")
+    void loginFail() {
+        // when
+        Member loginMember = memberService.login(testMember.getEmailId(), "wrongPassword");
+
+        // then
+        assertThat(loginMember).isNull();
     }
 
     @Test
-    void editInfo() {
+    @DisplayName("회원정보조회_성공")
+    void infoSuccess() {
+        // when
+        Member member = memberService.info(testMember.getId());
+
+        // then
+        assertThat(member).isNotNull();
+        assertThat(member.getEmailId()).isEqualTo(testMember.getEmailId());
+        assertThat(member.getNickName()).isEqualTo(testMember.getNickName());
     }
 
     @Test
+    @DisplayName("회원정보조회_실패")
+    void infoFail() {
+        // when
+        Member member = memberService.info(999L); // 존재하지 않는 ID
+
+        // then
+        assertThat(member).isNull();
+    }
+
+    @Test
+    @DisplayName("회원정보수정_성공")
     void edit() {
+        // given
+        MemberEditInfoDto memberEditInfoDto = new MemberEditInfoDto();
+        memberEditInfoDto.setNickName("수정된 유저");
+        memberEditInfoDto.setAge(30);
+        memberEditInfoDto.setGender("여자");
+        memberEditInfoDto.setAboutMe("안녕하세요 수정되었습니다");
+
+        // when
+        Member updatedMember = memberService.edit(testMember.getId(), memberEditInfoDto);
+
+        // then
+        assertThat(updatedMember).isNotNull();
+        assertThat(updatedMember.getNickName()).isEqualTo(memberEditInfoDto.getNickName());
+        assertThat(updatedMember.getAge()).isEqualTo(memberEditInfoDto.getAge());
+    }
+
+    @Test
+    @DisplayName("회원정보수정_실패")
+    void editFail() {
+        // given
+        MemberEditInfoDto memberEditInfoDto = new MemberEditInfoDto();
+        memberEditInfoDto.setNickName("수정된 유저");
+        memberEditInfoDto.setAge(30);
+        memberEditInfoDto.setGender("여자");
+        memberEditInfoDto.setAboutMe("안녕하세요 수정되었습니다");
+
+        // when
+        Member updatedMember = memberService.edit(999L, memberEditInfoDto); // 존재하지 않는 ID
+
+        // then
+        assertThat(updatedMember).isNull();
     }
 
     @Test
